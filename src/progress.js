a5b209acd95a2af7e8349fb6b5d05841e71394ce
Alexandra1003 <alexandrapanasenko10.03@gmail.com>
2019-01-29 16:14:20 +0200

    add download progress to title

--- src/progress.js
+++ src/progress.js
@@ -11,12 +11,34 @@ function onDownloadProgress(progressEvent) {
   if (progressEvent.lengthComputable) {
     const percentComplete = Math.round(progressEvent.loaded / progressEvent.total * 100);
     const progressBar = document.querySelector('.downloadBar');
+    const title = document.querySelector('title');
+
     progressBar.style.width = `${percentComplete}%`;
     progressBar.parentElement.classList.remove('transparent');
+    title.innerText = `Form ${percentComplete}%`;
 
     setTimeout(() => {
       progressBar.parentElement.classList.add('transparent');
       progressBar.style.width = '0%';
+      title.innerText = 'Form';
+    }, 2000);
+  }
+}
+
+function onUploadProgress(progressEvent) {
+  if (progressEvent.lengthComputable) {
+    const percentComplete = Math.round(progressEvent.loaded / progressEvent.total * 100);
+    const progressBar = document.querySelector('.uploadBar');
+
+    progressBar.style.width = `${percentComplete}%`;
+    progressBar.innerText = `${percentComplete}%`;
+    progressBar.parentElement.classList.remove('transparent');
+
+    loadAvailableFiles(); // eslint-disable-line
+    setTimeout(() => {
+      progressBar.parentElement.classList.add('transparent');
+      progressBar.style.width = '0%';
+      progressBar.innerText = '0%';
     }, 2000);
   }
 }
@@ -28,20 +50,4 @@ function onListItemClick(event) {
   event.target.classList.add('active');
   downloadInput.value = event.target.innerText; // eslint-disable-line
   enableButton(downloadButton); // eslint-disable-line
-}
-
-function onUploadProgress(progressEvent) {
-  if (progressEvent.lengthComputable) {
-    const percentComplete = Math.round(progressEvent.loaded / progressEvent.total * 100);
-    const progressBar = document.querySelector('.uploadBar');
-    progressBar.style.width = `${percentComplete}%`;
-    progressBar.innerText = `${percentComplete}%`;
-    progressBar.parentElement.classList.remove('transparent');
-    loadAvailableFiles(); // eslint-disable-line
-    setTimeout(() => {
-      progressBar.parentElement.classList.add('transparent');
-      progressBar.style.width = '0%';
-      progressBar.innerText = '0%';
-    }, 2000);
-  }
 }
